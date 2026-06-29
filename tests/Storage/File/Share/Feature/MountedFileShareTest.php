0f3ec1b423c6cfe88e88aaff9f2553b42f32ab58
brecht.vermeersch <brecht.vermeersch@doqmind.com>
2026-06-29 12:49:15 +0200

    speed up tests and show arnings

--- tests/Storage/File/Share/Feature/MountedFileShareTest.php
+++ tests/Storage/File/Share/Feature/MountedFileShareTest.php
@@ -4,39 +4,30 @@ declare(strict_types=1);
 
 namespace AzureOss\Tests\Storage\File\Share\Feature;
 
+use AzureOss\Tests\Storage\CreatesTempMountedShareDirectories;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class MountedFileShareTest extends TestCase
 {
+    use CreatesTempMountedShareDirectories;
+
     #[Test]
     public function mounted_file_share_supports_creating_a_directory_and_file(): void
     {
-        $mountPath = getenv('AZURE_STORAGE_FILE_SHARE_PATH');
-
-        if ($mountPath === false || $mountPath === '') {
-            self::markTestSkipped('Missing environment variable: AZURE_STORAGE_FILE_SHARE_PATH');
-        }
-
-        $root = rtrim($mountPath, '/').'/test-'.bin2hex(random_bytes(12));
-        $directory = $root.'/nested';
+        $root = $this->tempMountedShareDirectory();
+        $directory = $root['absolutePath'].'/nested';
         $filePath = $directory.'/hello.txt';
         $contents = 'Azure Files mounted share smoke test';
 
-        try {
-            self::assertTrue(mkdir($directory, 0777, true));
-            self::assertDirectoryExists($directory);
+        self::assertTrue(mkdir($directory, 0777, true));
+        self::assertDirectoryExists($directory);
 
-            $written = file_put_contents($filePath, $contents);
+        $written = file_put_contents($filePath, $contents);
 
-            self::assertNotFalse($written);
-            self::assertSame(strlen($contents), $written);
-            self::assertFileExists($filePath);
-            self::assertSame($contents, file_get_contents($filePath));
-        } finally {
-            @unlink($filePath);
-            @rmdir($directory);
-            @rmdir($root);
-        }
+        self::assertNotFalse($written);
+        self::assertSame(strlen($contents), $written);
+        self::assertFileExists($filePath);
+        self::assertSame($contents, file_get_contents($filePath));
     }
 }
--- tests/Storage/File/Share/Feature/ShareSasAuthorizationTest.php
+++ tests/Storage/File/Share/Feature/ShareSasAuthorizationTest.php
@@ -11,6 +11,7 @@ use AzureOss\Storage\File\Share\Sas\ShareSasBuilder;
 use AzureOss\Storage\File\Share\Sas\ShareSasPermissions;
 use AzureOss\Storage\File\Share\ShareClient;
 use AzureOss\Storage\File\Share\ShareFileClient;
+use AzureOss\Tests\Storage\CreatesTempMountedShareDirectories;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
 use PHPUnit\Framework\Attributes\Test;
@@ -18,108 +19,92 @@ use PHPUnit\Framework\TestCase;
 
 final class ShareSasAuthorizationTest extends TestCase
 {
+    use CreatesTempMountedShareDirectories;
+
     #[Test]
     public function file_sas_can_read_the_file_contents_from_azure_files(): void
     {
-        $mountPath = getenv('AZURE_STORAGE_FILE_SHARE_PATH');
         $shareName = getenv('AZURE_STORAGE_FILE_SHARE_NAME');
         $accountName = getenv('AZURE_STORAGE_FILE_SHARE_ACCOUNT_NAME');
         $accountKey = getenv('AZURE_STORAGE_FILE_SHARE_ACCOUNT_KEY');
 
         if (
-            $mountPath === false || $mountPath === ''
-            || $shareName === false || $shareName === ''
+            $shareName === false || $shareName === ''
             || $accountName === false || $accountName === ''
             || $accountKey === false || $accountKey === ''
         ) {
             self::markTestSkipped('Azure Files live-test environment variables are not configured.');
         }
 
-        $relativePath = 'sas-'.bin2hex(random_bytes(8)).'/hello.txt';
-        $directory = dirname($relativePath);
-        $absoluteDirectory = rtrim($mountPath, '/').'/'.$directory;
-        $absoluteFile = rtrim($mountPath, '/').'/'.$relativePath;
+        $directory = $this->tempMountedShareDirectory('sas-');
+        $relativePath = $directory['relativePath'].'/hello.txt';
+        $absoluteFile = $directory['absolutePath'].'/hello.txt';
         $contents = 'Azure Files SAS live test';
 
-        mkdir($absoluteDirectory, 0777, true);
         file_put_contents($absoluteFile, $contents);
 
-        try {
-            $file = new ShareFileClient(
-                new Uri("https://{$accountName}.file.core.windows.net/{$shareName}/{$relativePath}"),
-                new StorageSharedKeyCredential($accountName, $accountKey),
-            );
-            $sasUri = $file->generateSasUri(
-                ShareSasBuilder::new()
-                    ->setPermissions(new ShareFileSasPermissions(read: true))
-                    ->setVersion(ApiVersion::latestGA()->value)
-                    ->setExpiresOn(new \DateTimeImmutable('+10 minutes')),
-            );
+        $file = new ShareFileClient(
+            new Uri("https://{$accountName}.file.core.windows.net/{$shareName}/{$relativePath}"),
+            new StorageSharedKeyCredential($accountName, $accountKey),
+        );
+        $sasUri = $file->generateSasUri(
+            ShareSasBuilder::new()
+                ->setPermissions(new ShareFileSasPermissions(read: true))
+                ->setVersion(ApiVersion::latestGA()->value)
+                ->setExpiresOn(new \DateTimeImmutable('+10 minutes')),
+        );
 
-            $response = (new Client)->get($sasUri, [
-                'headers' => [
-                    'x-ms-version' => ApiVersion::latestGA()->value,
-                ],
-            ]);
+        $response = (new Client)->get($sasUri, [
+            'headers' => [
+                'x-ms-version' => ApiVersion::latestGA()->value,
+            ],
+        ]);
 
-            self::assertSame($contents, (string) $response->getBody());
-        } finally {
-            @unlink($absoluteFile);
-            @rmdir($absoluteDirectory);
-        }
+        self::assertSame($contents, (string) $response->getBody());
     }
 
     #[Test]
     public function share_sas_can_read_a_file_within_the_share_from_azure_files(): void
     {
-        $mountPath = getenv('AZURE_STORAGE_FILE_SHARE_PATH');
         $shareName = getenv('AZURE_STORAGE_FILE_SHARE_NAME');
         $accountName = getenv('AZURE_STORAGE_FILE_SHARE_ACCOUNT_NAME');
         $accountKey = getenv('AZURE_STORAGE_FILE_SHARE_ACCOUNT_KEY');
 
         if (
-            $mountPath === false || $mountPath === ''
-            || $shareName === false || $shareName === ''
+            $shareName === false || $shareName === ''
             || $accountName === false || $accountName === ''
             || $accountKey === false || $accountKey === ''
         ) {
             self::markTestSkipped('Azure Files live-test environment variables are not configured.');
         }
 
-        $relativePath = 'sas-share-'.bin2hex(random_bytes(8)).'/hello.txt';
-        $directory = dirname($relativePath);
-        $absoluteDirectory = rtrim($mountPath, '/').'/'.$directory;
-        $absoluteFile = rtrim($mountPath, '/').'/'.$relativePath;
+        $directory = $this->tempMountedShareDirectory('sas-share-');
+        $relativePath = $directory['relativePath'].'/hello.txt';
+        $absoluteFile = $directory['absolutePath'].'/hello.txt';
         $contents = 'Azure Files share SAS live test';
 
-        mkdir($absoluteDirectory, 0777, true);
         file_put_contents($absoluteFile, $contents);
 
-        try {
-            $share = new ShareClient(
-                new Uri("https://{$accountName}.file.core.windows.net/{$shareName}"),
-                new StorageSharedKeyCredential($accountName, $accountKey),
-            );
-            $shareSasUri = $share->generateSasUri(
-                ShareSasBuilder::new()
-                    ->setPermissions(new ShareSasPermissions(read: true))
-                    ->setVersion(ApiVersion::latestGA()->value)
-                    ->setExpiresOn(new \DateTimeImmutable('+10 minutes')),
-            );
-            $resourceUri = $shareSasUri
-                ->withPath($shareSasUri->getPath().'/'.$relativePath)
-                ->withQuery($shareSasUri->getQuery());
+        $share = new ShareClient(
+            new Uri("https://{$accountName}.file.core.windows.net/{$shareName}"),
+            new StorageSharedKeyCredential($accountName, $accountKey),
+        );
+        $shareSasUri = $share->generateSasUri(
+            ShareSasBuilder::new()
+                ->setPermissions(new ShareSasPermissions(read: true))
+                ->setVersion(ApiVersion::latestGA()->value)
+                ->setExpiresOn(new \DateTimeImmutable('+10 minutes')),
+        );
+        $resourceUri = $shareSasUri
+            ->withPath($shareSasUri->getPath().'/'.$relativePath)
+            ->withQuery($shareSasUri->getQuery());
 
-            $response = (new Client)->get($resourceUri, [
-                'headers' => [
-                    'x-ms-version' => ApiVersion::latestGA()->value,
-                ],
-            ]);
+        $response = (new Client)->get($resourceUri, [
+            'headers' => [
+                'x-ms-version' => ApiVersion::latestGA()->value,
+            ],
+        ]);
 
-            self::assertSame($contents, (string) $response->getBody());
-        } finally {
-            @unlink($absoluteFile);
-            @rmdir($absoluteDirectory);
-        }
+        self::assertSame($contents, (string) $response->getBody());
     }
 }
